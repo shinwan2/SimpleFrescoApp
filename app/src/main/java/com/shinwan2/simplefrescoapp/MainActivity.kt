176f0a5f6ed969c13eb289089497f4b00f710b45
shinwan2 <shinwan2@gmail.com>
2018-10-20 11:25:49 +0700

    Add Fresco library
    - Simplify View by removing ConstraintLayout
    - Add permission to use internet

--- app/src/main/java/com/shinwan2/simplefrescoapp/MainActivity.kt
+++ app/src/main/java/com/shinwan2/simplefrescoapp/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.shinwan2.simplefrescoapp
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.imageView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageView.setImageURI("http://static01.nyt.com/images/2015/08/18/business/18EMPLOY/18EMPLOY-thumbStandard.jpg")
     }
 }
